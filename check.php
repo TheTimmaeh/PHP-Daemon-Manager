87b97f606f0d67fa96cc64b1ddb657216d5990c3
Timmaeh <mail@timmaeh.de>
2014-09-26 20:48:15 +0200

    Installation setup
    Added a setup file for installation.
    Fixed status command for controller.

--- check.php
+++ check.php
@@ -1,31 +1,31 @@
 <?php
 
-	#Display all errors, output gets stored in log file
-	error_reporting('E_ALL');
+  #Display all errors, output gets stored in log file
+  error_reporting('E_ALL');
 
-	#Read Deamon configuration file
-	$daemons = parse_ini_file('daemons.ini', true);
-	
-	#Define path to 'service'
-	$service = '/usr/sbin/service';
-	
-	#Loop through Daemons found in configuration file
-	foreach($daemons as $daemon => $config){
+  #Read Deamon configuration file
+  $daemons = parse_ini_file('daemons.ini', true);
 
-		#Check whether Daemon controller exists
-        if(file_exists("/etc/init.d/{$daemon}")){
-		
-			#Get the status of the Daemon
-			$running = shell_exec("{$service} {$daemon} status");
+  #Define path to 'service'
+  $service = '/usr/sbin/service';
 
-			#Start Daemon, if not running, but enabled in configuration file
-			if(trim($running) != 'Running' && $config['enabled']) shell_exec("{$service} {$daemon} start");
-			
-			#Stop Daemon, if running, but disabled in configuration file
-			else if(trim($running) == 'Running' && !$config['enabled']) shell_exec("{$service} {$daemon} stop");
-			
-		}
+  #Loop through Daemons found in configuration file
+  foreach($daemons as $daemon => $config){
 
-	}
+    #Check whether Daemon controller exists
+    if(file_exists("/etc/init.d/{$daemon}")){
+
+      #Get the status of the Daemon
+      $status = shell_exec("{$service} {$daemon} status");
+
+      #Start Daemon, if not running, but enabled in configuration file
+      if(trim($status) != 'Online' && $config['enabled']) shell_exec("{$service} {$daemon} start");
+
+      #Stop Daemon, if running, but disabled in configuration file
+      else if(trim($status) == 'Online' && !$config['enabled']) shell_exec("{$service} {$daemon} stop");
+
+    }
+
+  }
 
 ?>
---
+++ jobs/mydaemon.php
@@ -0,0 +1,14 @@
+<?php
+
+  for($i = 1; $i <= 100000; $i++){
+
+    $content = "Loop-Run #$i\n";
+
+    file_put_contents("/daemons/logs/mydaemon.log", $content, FILE_APPEND | LOCK_EX);
+
+    sleep(5);
+
+  }
+
+?>
+
